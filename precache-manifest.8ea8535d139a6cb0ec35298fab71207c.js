a9ec6ff5bc2def5b2509219dc2ad87dbff6a5667
uppugundurulokesh <lokesh26.apssdc@gmail.com>
2020-04-27 19:38:44 +0530

    Updates

---
+++ precache-manifest.8ea8535d139a6cb0ec35298fab71207c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1c9be307d9315ab1e120054ad680bb",
+    "url": "/gopi-profile/index.html"
+  },
+  {
+    "revision": "9c2137bce99f4b59ee59",
+    "url": "/gopi-profile/static/css/main.44fd5715.chunk.css"
+  },
+  {
+    "revision": "7a736c070a5e7a6ad99d",
+    "url": "/gopi-profile/static/js/2.3274a4c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gopi-profile/static/js/2.3274a4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2137bce99f4b59ee59",
+    "url": "/gopi-profile/static/js/main.bf91aaa8.chunk.js"
+  },
+  {
+    "revision": "b34cb829b0bc473b66f6",
+    "url": "/gopi-profile/static/js/runtime-main.1bb94307.js"
+  }
+]);
